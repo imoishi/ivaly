0dedc01ddfcd917bc5323b40c10e2ee6278ae408
imoishi <cse.oishi@gmail.com>
2020-08-02 02:41:14 +0600

    create navber

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+
+// Sidebar
+
+function openFunction(){
+  console.log('paici oishi');
+  document.getElementById("menu").style.width="305px";
+  // document.getElementById("main-box").style.marginLeft="300px";
+}
+function closeFuntion(){
+  document.getElementById("menu").style.width="0px";
+  // document.getElementById("main-box").style.marginLeft="0px";
+}
+
+// $(document).ready(function(){
+//   $('.sideberbtn').click(function(){
+//     // console.log('paici oishi');
+//     $('.sideberbtn').toggleClass('active')
+//     // $('#logoImg').toggleClass('active')
+//     // $('#logoImg').addClass('active')
+//     // $('#logoImg').removeClass('active')
+//   })
+
+// })
+
+// $('.sideberbtn').on("click",function(){
+//   $('.sideberbtn').toggleClass('.close-btn');
+//   $('.sideberbtn').toggleClass('.side-menu');
+// });
